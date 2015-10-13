40d0e0d803008cad7704884942d4c366eb6f28a2
alex.brown <student@epicodus.com>
2015-10-13 10:02:01 -0700

    add blocks

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'alex_portfolio',
+      'username' => 'alex',
+      'password' => 'brown',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'NFOhWyYOg0PIlHR002XpTc4hVz49fpnCgOp8QdDx7So';
 
 /**
  * Base URL (optional).
